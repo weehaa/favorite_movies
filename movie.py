995900c1c16429cc37bf5b8ce1ad72ae94f4d5d4
weehaa <vzg1000@mail.ru>
2017-01-10 20:07:03 +0400

    nitial commit

---
+++ movie.py
@@ -0,0 +1,7 @@
+class Movie():
+    """This class stores inforamtion about Movie object"""
+    def __init__(self, title, storyline,  image, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
